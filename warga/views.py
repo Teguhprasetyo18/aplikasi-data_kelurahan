e52dd7b4f38e52b4ceb3ae996f9cb7c48a37dd8a
teguh <teguh89.prasetyo@gmail.com>
2025-10-05 22:22:37 +0700

    first commit

---
+++ warga/views.py
@@ -0,0 +1,9 @@
+from django.views.generic import ListView, DetailView
+from .models import Warga
+
+# Create your views here.
+class WargaListView(ListView):
+    model = Warga
+
+class WargaDetailView(DetailView):
+    model = Warga
