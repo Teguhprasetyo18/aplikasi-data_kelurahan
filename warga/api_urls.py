b7094339d1d91bfdd7d1cafd8e248f304e1e41a5
Teguhprasetyo18 <teguh89.prasetyo@gmail.com>
2025-10-31 13:25:07 +0700

    api_urls.py

--- warga/api_urls.py
+++ warga/api_urls.py
@@ -3,4 +3,6 @@ from .views import WargaListAPIView
 
 urlpatterns = [
     path('warga/', WargaListAPIView.as_view(), name='api-warga-list'),
+    path('warga/tambah/', WargaCreateAPIView.as_view(), name='warga-create'),
+    path('warga/<int:pk>/', WargaDetailAPIView.as_view(), name='warga-detail'),
 ]
