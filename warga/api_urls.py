268ee9cb5158cfb822070c0f0cc7bde73fd85d98
Teguhprasetyo18 <teguh89.prasetyo@gmail.com>
2025-11-06 01:59:53 +0700

    Update api_urls.py

--- warga/api_urls.py
+++ warga/api_urls.py
@@ -1,15 +1,17 @@
 from django.urls import path, include
-from .views import WargaListAPIView, WargaCreateAPIView, WargaDetailAPIView
+#from .views import WargaListAPIView, WargaCreateAPIView, WargaDetailAPIView
 from rest_framework.routers import DefaultRouter
 from .views import PengaduanViewSet
 router = DefaultRouter()
 router.register(r'pengaduan', PengaduanViewSet, basename='pengaduan')
-
+router.register(r'pengaduan', PengaduanViewSet, basename='pengaduan')
+router.register('login', LoginViewSet, basename='login')
 
 urlpatterns = [
-    path('warga/', WargaListAPIView.as_view(), name='api-warga-list'),
-    path('warga/tambah/', WargaCreateAPIView.as_view(), name='warga-create'),
-    path('warga/<int:pk>/', WargaDetailAPIView.as_view(), name='warga-detail'),
+    #path('warga/', WargaListAPIView.as_view(), name='api-warga-list'),
+    #path('warga/tambah/', WargaCreateAPIView.as_view(), name='warga-create'),
+    #path('warga/<int:pk>/', WargaDetailAPIView.as_view(), name='warga-detail'),
+    # path('', api_root, name='api-root'),   # ⬅️ ini baris penting!
     path('', include(router.urls)),
 
 ]
