c3000fe48f27963e6a6dfb01c1e976f9d4b979a8
Teguhprasetyo18 <teguh89.prasetyo@gmail.com>
2025-11-05 21:43:24 +0700

    Update urls.py

--- warga/urls.py
+++ warga/urls.py
@@ -1,16 +1,16 @@
 from django.urls import path
-from .views import WargaListView, WargaDetailView, PengaduanListView, WargaCreateView,WargaUpdateView,WargaDeleteView,PengaduanUpdateView,PengaduanDeleteView
-from .views import WargaListAPIView,WargaDetailAPIView
+from .views import WargaListView, WargaDetailView, PengaduanListView, WargaCreateView, PengaduanCreateView, WargaUpdateView, WargaDeleteView, PengaduanDeleteView, PengaduanUpdateView, PengaduanDetailView
+
 
 urlpatterns = [
-    path('', WargaListView.as_view(), name='warga-list'),
-    path('tambah/', WargaCreateView.as_view(), name='warga-tambah'), 
-    path('<int:pk>/', WargaDetailView.as_view(), name='warga-detail'),
-    path('pengaduan/', PengaduanListView.as_view(), name='pengaduan_list'),
-    path('<int:pk>/edit/', WargaUpdateView.as_view(), name='warga-edit'), 
-    path('<int:pk>/hapus/', WargaDeleteView.as_view(), name='warga-hapus'),
-    path('<int:pk>/edit/', PengaduanUpdateView.as_view(), name='Pengaduan-edit'), 
-    path('<int:pk>/hapus/', PengaduanDeleteView.as_view(), name='Pengaduan-hapus'),
-    path('warga/', WargaListAPIView.as_view(), name='api-warga-list'),
-    path('api/warga/<int:pk>/', WargaDetailAPIView.as_view(), name='warga-detail'),
+    path('', WargaListView.as_view(), name = 'warga-list'),
+    path('<int:pk>/', WargaDetailView.as_view(), name = 'warga-detail'),
+    path('tambah/', WargaCreateView.as_view(), name='warga-tambah'), # URL untuk form tambah
+    path('pengaduan/', PengaduanListView.as_view(), name='pengaduan-list'),
+    path('pengaduan/tambah', PengaduanCreateView.as_view(), name='pengaduan-tambah'),
+    path('<int:pk>/edit/', WargaUpdateView.as_view(), name='warga-edit'), # URL untuk edit
+    path('<int:pk>/hapus/', WargaDeleteView.as_view(), name='warga-hapus'), # URL untuk hapus
+    path('pengaduan/<int:pk>/edit/', PengaduanUpdateView.as_view(), name='pengaduan-edit'),
+    path('pengaduan/<int:pk>/hapus/', PengaduanDeleteView.as_view(), name='pengaduan-hapus'),
+    path('pengaduan/<int:pk>/', PengaduanDetailView.as_view(), name='pengaduan-detail'),
 ]
