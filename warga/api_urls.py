5e5c3bac12c3b1045af99d83c7caca351a5f569f
Teguhprasetyo18 <teguh89.prasetyo@gmail.com>
2025-11-12 22:10:12 +0700

    Update api_urls.py

--- warga/api_urls.py
+++ warga/api_urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
     #path('warga/', WargaListAPIView.as_view(), name='api-warga-list'),
     #path('warga/tambah/', WargaCreateAPIView.as_view(), name='warga-create'),
     #path('warga/<int:pk>/', WargaDetailAPIView.as_view(), name='warga-detail'),
-    # path('', api_root, name='api-root'),   # ⬅️ ini baris penting!
+    #path('', api_root, name='api-root'),   # ⬅️ ini baris penting!
     path('', include(router.urls)),
 
 ]
