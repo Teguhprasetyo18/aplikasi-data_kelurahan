3263568898116f6a59cf9599b096f45086b82265
Teguhprasetyo18 <teguh89.prasetyo@gmail.com>
2025-11-13 08:18:58 +0700

    Update settings.py

--- data_kelurahan/settings.py
+++ data_kelurahan/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'warga',
-    'rest_framework',
+    'rest_framework'
+    'rest_framework.authtoken', # Tambahkan ini
 ]
 
 MIDDLEWARE = [
