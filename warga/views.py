a9764d88cdd49a95b59a16f68736202be3aeafb0
Teguhprasetyo18 <teguh89.prasetyo@gmail.com>
2025-11-17 13:30:24 +0700

    Pertemuan 9

--- warga/views.py
+++ warga/views.py
@@ -3,9 +3,15 @@ from django.urls import reverse_lazy
 from .models import Warga, Pengaduan
 from .forms import WargaForm, PengaduanForm
 from rest_framework.generics import ListAPIView, CreateAPIView, RetrieveAPIView
-from rest_framework import viewsets
-from .serializers import WargaSerializer, PengaduanSerializer    
+from rest_framework import viewsets,status # Impor viewsets
+from .serializers import WargaSerializer, PengaduanSerializer,LoginSerializer    
 from django.views.generic import ListView,DetailView,CreateView,UpdateView,DeleteView
+from rest_framework.response import Response
+from rest_framework.authtoken.models import Token
+from rest_framework.permissions import AllowAny
+from django.shortcuts import redirect
+from rest_framework.decorators import api_view
+
 
 
 class PengaduanViewSet(viewsets.ModelViewSet):
@@ -80,6 +86,45 @@ class WargaCreateAPIView(CreateAPIView):
 class WargaDetailAPIView(RetrieveAPIView):
     queryset = Warga.objects.all()
     serializer_class = WargaSerializer
+
 class WargaViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
     queryset = Warga.objects.all().order_by('-tanggal_registrasi')
     serializer_class = WargaSerializer
+
+class PengaduanViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = Pengaduan.objects.all().order_by('-tanggal_lapor')
+    serializer_class = PengaduanSerializer
+
+class LoginViewSet(viewsets.ViewSet):
+    permission_classes = [AllowAny]
+
+    def create(self, request):
+        serializer = LoginSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.validated_data['user']
+            token, created = Token.objects.get_or_create(user=user)
+            return Response({
+                'token': token.key,
+                'user': {
+                    'id': user.id,
+                    'username': user.username,
+                    'email': user.email,
+                }
+            })
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    
+
+# @api_view(['GET'])
+# def api_root(request):
+#     # Jika user belum login → redirect ke login
+#     if not request.user.is_authenticated:
+#         return redirect('/api/login/')
+#     # Kalau sudah login, bisa tampilkan pesan lain atau data awal
+#     return Response({"message": "Selamat datang di API Warga!"}, status=status.HTTP_200_OK)
